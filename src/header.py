595d2b46182cbf4fdf2c638fd852975d2a27c312
Simon Yu <jundayu2@illinois.edu>
2022-12-17 19:56:48 -0600

    Add filter script

---
+++ src/header.py
@@ -0,0 +1,24 @@
+import logger
+
+dataset_dir = "../data"
+dataset_annotations_dir = dataset_dir + "/annotations"
+dataset_images_dir = dataset_dir + "/images"
+dataset_splits_dir = dataset_dir + "/splits"
+
+filter_file_name = "filter.txt"
+filter_print_params = True
+filter_frame_ispano = 0
+filter_object_keys = []
+filter_object_labels = []
+filter_object_properties = False
+filter_object_barrier = 0
+filter_object_occluded = 0
+filter_object_out_of_frame = 0
+filter_object_exterior = 0
+filter_object_ambiguous = 0
+filter_object_included = 0
+filter_object_direction_or_information = 0
+filter_object_highway = 0
+filter_object_dummy = 0
+
+log_level = logger.LogLevel.trace
