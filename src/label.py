fcd5aaf194001109f2880906b92a47a4dcf1bef4
Simon Yu <jundayu2@illinois.edu>
2023-01-22 17:46:48 -0600

    Clean up

--- src/label.py
+++ src/label.py
@@ -7,12 +7,13 @@ import os
 import PyQt5.QtCore
 import PyQt5.QtGui
 import PyQt5.QtWidgets
+import sys
 
 file_config = open(os.path.join(header.config_dir, header.label_config_file_name), "r")
 config = json.load(file_config)
 file_config.close()
 
-def addLabel(dataset_name, combo_box, line_edit):
+def addLabelSlot(dataset_name, combo_box, line_edit):
     label = line_edit.text().lower()
 
     if (label == dataset_name + "--" or
@@ -30,7 +31,7 @@ def addLabel(dataset_name, combo_box, line_edit):
 
     return
 
-def removeLabel(dataset_name, combo_box):
+def removeLabelSlot(dataset_name, combo_box):
     label = combo_box.currentText()
 
     if label == "":
@@ -47,7 +48,7 @@ def removeLabel(dataset_name, combo_box):
 
     return
 
-def createViewer():
+def createViewerWidget():
     group_box_viewer = PyQt5.QtWidgets.QGroupBox()
     layout_viewer = PyQt5.QtWidgets.QGridLayout()
 
@@ -64,13 +65,13 @@ def createViewer():
 
     return group_box_viewer
 
-def createLabelingControls():
-    group_box_labeling_controls = PyQt5.QtWidgets.QGroupBox()
-    layout_labeling_controls = PyQt5.QtWidgets.QGridLayout()
+def createLabelingControlWidget():
+    group_box_labeling_control = PyQt5.QtWidgets.QGroupBox()
+    layout_labeling_control = PyQt5.QtWidgets.QGridLayout()
 
-    group_box_labeling_controls.setAlignment(PyQt5.QtCore.Qt.AlignHCenter)
-    group_box_labeling_controls.setLayout(layout_labeling_controls)
-    group_box_labeling_controls.setTitle("Labeling Controls")
+    group_box_labeling_control.setAlignment(PyQt5.QtCore.Qt.AlignHCenter)
+    group_box_labeling_control.setLayout(layout_labeling_control)
+    group_box_labeling_control.setTitle("Labeling Control")
 
     for dataset in config["datasets"]:
         combo_box = PyQt5.QtWidgets.QComboBox()
@@ -90,19 +91,19 @@ def createLabelingControls():
         layout_group_box_control.addWidget(line_edit, 0, 1)
         layout_group_box_control.addWidget(push_button_add_label, 0, 2)
         layout_group_box_control.addWidget(push_button_remove_label, 0, 3)
-        layout_labeling_controls.addWidget(group_box_control)
+        layout_labeling_control.addWidget(group_box_control)
         line_edit.setFixedWidth(header.label_line_edit_width)
         line_edit.setText(dataset["name"] + "--")
-        push_button_add_label.clicked.connect(functools.partial(addLabel, dataset["name"], combo_box, line_edit))
+        push_button_add_label.clicked.connect(functools.partial(addLabelSlot, dataset["name"], combo_box, line_edit))
         push_button_add_label.setText("Add Label")
-        push_button_remove_label.clicked.connect(functools.partial(removeLabel, dataset["name"], combo_box))
+        push_button_remove_label.clicked.connect(functools.partial(removeLabelSlot, dataset["name"], combo_box))
         push_button_remove_label.setText("Remove Label")
 
-    return group_box_labeling_controls
+    return group_box_labeling_control
 
-def createApplicationControls():
-    group_box_application_controls = PyQt5.QtWidgets.QGroupBox()
-    layout_application_controls = PyQt5.QtWidgets.QGridLayout()
+def createApplicationControlWidget():
+    group_box_application_control = PyQt5.QtWidgets.QGroupBox()
+    layout_application_control = PyQt5.QtWidgets.QGridLayout()
     push_button_load = PyQt5.QtWidgets.QPushButton()
     push_button_last = PyQt5.QtWidgets.QPushButton()
     push_button_next = PyQt5.QtWidgets.QPushButton()
@@ -110,15 +111,15 @@ def createApplicationControls():
     push_button_save_and_last = PyQt5.QtWidgets.QPushButton()
     push_button_save_and_next = PyQt5.QtWidgets.QPushButton()
 
-    group_box_application_controls.setAlignment(PyQt5.QtCore.Qt.AlignHCenter)
-    group_box_application_controls.setLayout(layout_application_controls)
-    group_box_application_controls.setTitle("Application Controls")
-    layout_application_controls.addWidget(push_button_save_and_last, 0, 1)
-    layout_application_controls.addWidget(push_button_last, 0, 2)
-    layout_application_controls.addWidget(push_button_load, 0, 3)
-    layout_application_controls.addWidget(push_button_save, 0, 4)
-    layout_application_controls.addWidget(push_button_next, 0, 5)
-    layout_application_controls.addWidget(push_button_save_and_next, 0, 6)
+    group_box_application_control.setAlignment(PyQt5.QtCore.Qt.AlignHCenter)
+    group_box_application_control.setLayout(layout_application_control)
+    group_box_application_control.setTitle("Application Control")
+    layout_application_control.addWidget(push_button_save_and_last, 0, 1)
+    layout_application_control.addWidget(push_button_last, 0, 2)
+    layout_application_control.addWidget(push_button_load, 0, 3)
+    layout_application_control.addWidget(push_button_save, 0, 4)
+    layout_application_control.addWidget(push_button_next, 0, 5)
+    layout_application_control.addWidget(push_button_save_and_next, 0, 6)
     push_button_load.setText("Load")
     push_button_last.setText("Last")
     push_button_next.setText("Next")
@@ -126,29 +127,29 @@ def createApplicationControls():
     push_button_save_and_last.setText("Save && Last")
     push_button_save_and_next.setText("Save && Next")
 
-    return group_box_application_controls
+    return group_box_application_control
+
+def createWindowLayout():
+    layout_window = PyQt5.QtWidgets.QGridLayout()
+    splitter = PyQt5.QtWidgets.QSplitter()
+
+    layout_window.addWidget(splitter)
+    splitter.addWidget(createViewerWidget())
+    splitter.addWidget(createLabelingControlWidget())
+    splitter.addWidget(createApplicationControlWidget())
+    splitter.setOrientation(PyQt5.QtCore.Qt.Vertical)
+
+    return layout_window
 
 def main():
     application = PyQt5.QtWidgets.QApplication([])
-
-    layout_window = PyQt5.QtWidgets.QGridLayout()
-    splitter = PyQt5.QtWidgets.QSplitter()
     window = PyQt5.QtWidgets.QWidget()
 
-    splitter.setOrientation(PyQt5.QtCore.Qt.Vertical)
+    window.setLayout(createWindowLayout())
     window.setWindowTitle("Mapillary Dataset Labeling Tool")
-    window.setLayout(layout_window)
-
-    layout_window.addWidget(splitter)
-    splitter.addWidget(createViewer())
-    splitter.addWidget(createLabelingControls())
-    splitter.addWidget(createApplicationControls())
-
     window.show()
 
-    status = application.exec()
-
-    return status
+    sys.exit(application.exec())
 
 if __name__ == "__main__":
     main()
