46be70f233515909e0801911b203af5f4adb568b
Simon Yu <jundayu2@illinois.edu>
2023-01-22 16:31:41 -0600

    Initialize pixmaps

--- src/label.py
+++ src/label.py
@@ -2,7 +2,6 @@
 
 import header
 import json
-import logger
 import os
 import PyQt5.QtCore
 import PyQt5.QtGui
@@ -12,28 +11,29 @@ file_config = open(os.path.join(header.config_dir, header.label_config_file_name
 config = json.load(file_config)
 
 def createViewer():
-    group_box = PyQt5.QtWidgets.QGroupBox()
-    layout = PyQt5.QtWidgets.QGridLayout()
+    group_box_viewer = PyQt5.QtWidgets.QGroupBox()
+    layout_viewer = PyQt5.QtWidgets.QGridLayout()
 
-    group_box.setAlignment(PyQt5.QtCore.Qt.AlignHCenter)
-    group_box.setLayout(layout)
-    group_box.setTitle("Viewer")
+    group_box_viewer.setAlignment(PyQt5.QtCore.Qt.AlignHCenter)
+    group_box_viewer.setLayout(layout_viewer)
+    group_box_viewer.setTitle("Viewer")
 
     for i in range(0, header.label_viewer_items):
         label = PyQt5.QtWidgets.QLabel()
         pixmap = PyQt5.QtGui.QPixmap(header.label_viewer_width, header.label_viewer_height)
+        pixmap.fill(PyQt5.QtCore.Qt.black)
         label.setPixmap(pixmap)
-        layout.addWidget(label, i // header.label_viewer_items_row, i % header.label_viewer_items_row)
+        layout_viewer.addWidget(label, i // header.label_viewer_items_row, i % header.label_viewer_items_row)
 
-    return group_box
+    return group_box_viewer
 
 def createLabelingControls():
-    group_box_controls = PyQt5.QtWidgets.QGroupBox()
-    layout_group_box_controls = PyQt5.QtWidgets.QGridLayout()
+    group_box_labeling_controls = PyQt5.QtWidgets.QGroupBox()
+    layout_labeling_controls = PyQt5.QtWidgets.QGridLayout()
 
-    group_box_controls.setAlignment(PyQt5.QtCore.Qt.AlignHCenter)
-    group_box_controls.setLayout(layout_group_box_controls)
-    group_box_controls.setTitle("Labeling Controls")
+    group_box_labeling_controls.setAlignment(PyQt5.QtCore.Qt.AlignHCenter)
+    group_box_labeling_controls.setLayout(layout_labeling_controls)
+    group_box_labeling_controls.setTitle("Labeling Controls")
 
     for dataset in config["datasets"]:
         combo_box = PyQt5.QtWidgets.QComboBox()
@@ -53,17 +53,17 @@ def createLabelingControls():
         layout_group_box_control.addWidget(line_edit, 0, 1)
         layout_group_box_control.addWidget(push_button_add_label, 0, 2)
         layout_group_box_control.addWidget(push_button_remove_label, 0, 3)
-        layout_group_box_controls.addWidget(group_box_control)
+        layout_labeling_controls.addWidget(group_box_control)
         line_edit.setFixedWidth(header.label_line_edit_width)
         line_edit.setText(dataset["name"] + "--")
         push_button_add_label.setText("Add Label")
         push_button_remove_label.setText("Remove Label")
 
-    return group_box_controls
+    return group_box_labeling_controls
 
 def createApplicationControls():
-    group_box = PyQt5.QtWidgets.QGroupBox()
-    layout = PyQt5.QtWidgets.QGridLayout()
+    group_box_application_controls = PyQt5.QtWidgets.QGroupBox()
+    layout_application_controls = PyQt5.QtWidgets.QGridLayout()
     push_button_load = PyQt5.QtWidgets.QPushButton()
     push_button_last = PyQt5.QtWidgets.QPushButton()
     push_button_next = PyQt5.QtWidgets.QPushButton()
@@ -71,15 +71,15 @@ def createApplicationControls():
     push_button_save_and_last = PyQt5.QtWidgets.QPushButton()
     push_button_save_and_next = PyQt5.QtWidgets.QPushButton()
 
-    group_box.setAlignment(PyQt5.QtCore.Qt.AlignHCenter)
-    group_box.setLayout(layout)
-    group_box.setTitle("Application Controls")
-    layout.addWidget(push_button_save_and_last, 0, 1)
-    layout.addWidget(push_button_last, 0, 2)
-    layout.addWidget(push_button_load, 0, 3)
-    layout.addWidget(push_button_save, 0, 4)
-    layout.addWidget(push_button_next, 0, 5)
-    layout.addWidget(push_button_save_and_next, 0, 6)
+    group_box_application_controls.setAlignment(PyQt5.QtCore.Qt.AlignHCenter)
+    group_box_application_controls.setLayout(layout_application_controls)
+    group_box_application_controls.setTitle("Application Controls")
+    layout_application_controls.addWidget(push_button_save_and_last, 0, 1)
+    layout_application_controls.addWidget(push_button_last, 0, 2)
+    layout_application_controls.addWidget(push_button_load, 0, 3)
+    layout_application_controls.addWidget(push_button_save, 0, 4)
+    layout_application_controls.addWidget(push_button_next, 0, 5)
+    layout_application_controls.addWidget(push_button_save_and_next, 0, 6)
     push_button_load.setText("Load")
     push_button_last.setText("Last")
     push_button_next.setText("Next")
@@ -87,7 +87,7 @@ def createApplicationControls():
     push_button_save_and_last.setText("Save && Last")
     push_button_save_and_next.setText("Save && Next")
 
-    return group_box
+    return group_box_application_controls
 
 def main():
     application = PyQt5.QtWidgets.QApplication([])
