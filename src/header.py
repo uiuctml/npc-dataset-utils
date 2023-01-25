f317f59f29c1c159ca1202f5b46eecbb76500706
Simon Yu <jundayu2@illinois.edu>
2023-01-25 17:03:20 -0600

    Create split symlinks

--- src/header.py
+++ src/header.py
@@ -10,6 +10,7 @@ dataset_dir_annotations_original = dataset_dir_annotations + "/original"
 dataset_dir_images = dataset_dir + "/images"
 dataset_dir_images_original = dataset_dir_images + "/original"
 dataset_dir_images_sliced = dataset_dir_images + "/sliced"
+dataset_dir_images_split = dataset_dir_images + "/split"
 dataset_dir_splits = dataset_dir + "/splits"
 dataset_dir_splits_resplit = dataset_dir_splits + "/resplit"
 dataset_dir_splits_object = dataset_dir_splits + "/object"
@@ -61,6 +62,7 @@ slice_shared_memory_size_process = 256
 slice_shared_memory_size_total = slice_shared_memory_size_process * (slice_process_count + 1)
 
 split_dataset_dir_annotations = dataset_dir_annotations_original
+split_dataset_dir_images = dataset_dir_images_sliced + "/original"
 split_resplit_training_percentage_training = 0.8
 split_resplit_training_percentage_validation = 1 - split_resplit_training_percentage_training
 
--- src/split.py
+++ src/split.py
@@ -9,6 +9,8 @@ import tqdm
 def resplit():
     if not os.path.isdir(header.dataset_dir_splits_resplit):
         os.makedirs(header.dataset_dir_splits_resplit, exist_ok = True)
+    else:
+        return
 
     file_split_original_train = open(os.path.join(header.dataset_dir_splits_original, header.dataset_file_name_split_train), "r")
     file_split_original_train_lines = file_split_original_train.readlines()
@@ -33,6 +35,9 @@ def resplit():
     return
 
 def createObjectSplits(dataset_file_name_split):
+    if os.path.isfile(os.path.join(header.dataset_dir_splits_object, dataset_file_name_split)):
+        return
+
     file_split_resplit = open(os.path.join(header.dataset_dir_splits_resplit, dataset_file_name_split), "r")
     file_split_resplit_lines = file_split_resplit.readlines()
     file_split_resplit.close()
@@ -68,12 +73,55 @@ def createObjectSplits(dataset_file_name_split):
 
     return
 
+def createSplitSymlinks(dataset_file_name_split):
+    dataset_dir_images_list = os.listdir(header.split_dataset_dir_images)
+    dataset_name_split = dataset_file_name_split.split(".")[0]
+
+    if os.path.isdir(os.path.join(header.dataset_dir_images_split, dataset_name_split)):
+        return
+
+    file_split_object = open(os.path.join(header.dataset_dir_splits_object, dataset_file_name_split), "r")
+    file_split_object_lines = file_split_object.readlines()
+    file_split_object.close()
+
+    for i in range(0, len(file_split_object_lines)):
+        file_split_object_lines[i] = file_split_object_lines[i].strip()
+
+    file_split_object_lines = set(file_split_object_lines)
+
+    dataset_dir_labels_counter = 1
+    progress_bar = tqdm.tqdm(total = len(dataset_dir_images_list))
+
+    for dataset_dir_labels in dataset_dir_images_list:
+        dataset_dir_labels_split = os.path.join(header.dataset_dir_images_split, dataset_name_split, dataset_dir_labels)
+        progress_bar.set_description_str("Processing \"" + dataset_dir_labels + "\" for \"" + dataset_name_split + "\" dataset split")
+        progress_bar.n = dataset_dir_labels_counter
+        progress_bar.refresh()
+
+        dataset_dir_labels_counter += 1
+        dataset_images_list = os.listdir(os.path.join(header.split_dataset_dir_images, dataset_dir_labels))
+
+        if not os.path.isdir(dataset_dir_labels_split):
+            os.makedirs(dataset_dir_labels_split, exist_ok = True)
+
+        for file_name_image in dataset_images_list:
+            file_key = file_name_image.split(".")[0]
+
+            if file_key in file_split_object_lines:
+                os.symlink(os.path.join(header.split_dataset_dir_images, dataset_dir_labels, file_name_image), os.path.join(dataset_dir_labels_split, file_name_image))
+
+    return
+
 def main():
     resplit()
 
     createObjectSplits(header.dataset_file_name_split_train)
     createObjectSplits(header.dataset_file_name_split_validation)
     createObjectSplits(header.dataset_file_name_split_test)
+
+    createSplitSymlinks(header.dataset_file_name_split_train)
+    createSplitSymlinks(header.dataset_file_name_split_validation)
+    createSplitSymlinks(header.dataset_file_name_split_test)
 
     return
 
