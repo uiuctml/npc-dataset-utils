6e915a204b15bb15f24156676a9867b7c8222d9a
Simon Yu <jundayu2@illinois.edu>
2023-01-26 22:30:20 -0600

    Clear progress bars after done

--- src/slice.py
+++ src/slice.py
@@ -46,7 +46,7 @@ def monitor():
         file_annotations_counter = int(readFromSharedMemory(shared_memory, process_id, 1))
         dataset_dir_annotations_list_size = int(readFromSharedMemory(shared_memory, process_id, 2))
 
-        progress_bar = tqdm.tqdm(total = dataset_dir_annotations_list_size, position = process_id)
+        progress_bar = tqdm.tqdm(total = dataset_dir_annotations_list_size, position = process_id, leave = False)
         progress_bar.set_description_str("Processing \"" + file_name_annotations + "\"")
         progress_bars.append(progress_bar)
 
