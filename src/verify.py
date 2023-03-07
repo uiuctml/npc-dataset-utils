6ad3f81cbcf263ad1cc5f003f10de49ed1b9aa82
Simon Yu <jundayu2@illinois.edu>
2023-03-06 18:23:02 -0600

    Check duplicates in generate config

--- src/verify.py
+++ src/verify.py
@@ -6,6 +6,8 @@ import logger
 import os
 
 def verifyDatasetConfig():
+
+
     return
 
 def verifyDatasetSplits():
@@ -26,57 +28,90 @@ def verifyDatasetSplits():
 
     return
 
-def verifyGenerateConfigLabels(generate_config, label):
-    if len(generate_config[label]["labels"]) == 0:
-        logger.log_error("\"" + label + "\" does not contain any labels.")
-    else:
-        for dataset_name in generate_config[label]["labels"].keys():
-            if generate_config[label]["labels"][dataset_name] == "":
-                logger.log_error("\"" + label + "\" contains an empty label for dataset \"" + dataset_name + "\".")
-            elif generate_config[label]["labels"][dataset_name].split(header.dataset_delimiter_label)[0] != dataset_name:
-                logger.log_error("\"" + label + "\" contains an invalid label for dataset \"" + dataset_name + "\".")
+def verifyGenerateConfigDuplicates(config_generate):
+    label_map = {}
+
+    for label_original in config_generate.keys():
+        if len(config_generate[label_original]["labels"]) != 0:
+            labels_dataset = ""
+
+            for dataset_name in config_generate[label_original]["labels"].keys():
+                label_dataset = config_generate[label_original]["labels"][dataset_name]
+
+                if labels_dataset != "":
+                    labels_dataset += header.dataset_delimiter_file_name
+
+                labels_dataset += label_dataset
+
+            if labels_dataset not in label_map:
+                label_map[labels_dataset] = [label_original]
+            else:
+                label_map[labels_dataset].append(label_original)
+
+    for label_dataset in label_map.keys():
+        labels_original_list = label_map[label_dataset]
+
+        if len(labels_original_list) > 1:
+            labels_original = ", ".join(labels_original_list)
+            logger.log_error("\"" + labels_original + "\" contain identical labels.")
 
     return
 
-def verifyGenerateConfigWeights(generate_config, label):
-    if len(generate_config[label]["weights"]) == 0:
-            logger.log_error("\"" + label + "\" does not contain any weights.")
-    else:
-        weights_sum = 0
-
-        for dataset_name in generate_config[label]["weights"].keys():
-            weights_sum += generate_config[label]["weights"][dataset_name]
-
-        if weights_sum == 0:
-            logger.log_error("\"" + label + "\" contains zero weights.")
+def verifyGenerateConfigLabels(config_generate):
+    for label_original in config_generate.keys():
+        if len(config_generate[label_original]["labels"]) == 0:
+            logger.log_error("\"" + label_original + "\" does not contain any labels.")
+        else:
+            for dataset_name in config_generate[label_original]["labels"].keys():
+                if config_generate[label_original]["labels"][dataset_name] == "":
+                    logger.log_error("\"" + label_original + "\" contains an empty label for dataset \"" + dataset_name + "\".")
+                elif config_generate[label_original]["labels"][dataset_name].split(header.dataset_delimiter_label)[0] != dataset_name:
+                    logger.log_error("\"" + label_original + "\" contains an invalid label for dataset \"" + dataset_name + "\".")
 
     return
 
-def verifyGenerateConfigReductions(generate_config, label):
-    reduction = generate_config[label]["reduction"]
+def verifyGenerateConfigWeights(config_generate):
+    for label_original in config_generate.keys():
+        if len(config_generate[label_original]["weights"]) == 0:
+                logger.log_error("\"" + label_original + "\" does not contain any weights.")
+        else:
+            weights_sum = 0
 
-    if reduction == "":
-        logger.log_error("\"" + label + "\" contains an empty reduction.")
+            for dataset_name in config_generate[label_original]["weights"].keys():
+                weights_sum += config_generate[label_original]["weights"][dataset_name]
+
+            if weights_sum == 0:
+                logger.log_error("\"" + label_original + "\" contains zero weights.")
+
+    return
+
+def verifyGenerateConfigReductions(config_generate):    
+    for label_original in config_generate.keys():
+        reduction = config_generate[label_original]["reduction"]
+
+        if reduction == "":
+            logger.log_error("\"" + label_original + "\" contains an empty reduction.")
 
     return
 
 def verifyGenerateConfig():
-    file_path_generate_config = os.path.join(header.config_dir, header.generate_config_file_name)
+    file_path_config_generate = os.path.join(header.config_dir, header.generate_config_file_name)
 
-    logger.log_info("Verifying \"" + file_path_generate_config + "\"...")
+    logger.log_info("Verifying \"" + file_path_config_generate + "\"...")
 
-    file_generate_config = open(file_path_generate_config, "r")
-    generate_config = json.load(file_generate_config)
-    file_generate_config.close()
+    file_config_generate = open(file_path_config_generate, "r")
+    config_generate = json.load(file_config_generate)
+    file_config_generate.close()
 
-    for label in generate_config.keys():
-        verifyGenerateConfigLabels(generate_config, label)
-        verifyGenerateConfigWeights(generate_config, label)
-        verifyGenerateConfigReductions(generate_config, label)
+    verifyGenerateConfigDuplicates(config_generate)
+    verifyGenerateConfigLabels(config_generate)
+    verifyGenerateConfigWeights(config_generate)
+    verifyGenerateConfigReductions(config_generate)
 
     return
 
 def main():
+    verifyDatasetConfig()
     verifyDatasetSplits()
     verifyGenerateConfig()
 
