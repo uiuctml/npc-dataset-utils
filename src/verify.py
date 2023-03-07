bc0b53d809b025c8341b1b99ab44d7509616a7b5
Simon Yu <jundayu2@illinois.edu>
2023-03-06 18:00:06 -0600

    Break down verifyGenerateConfig

--- src/verify.py
+++ src/verify.py
@@ -5,46 +5,10 @@ import json
 import logger
 import os
 
-def main():
-    file_path_config = os.path.join(header.config_dir, header.generate_config_file_name)
+def verifyDatasetConfig():
+    return
 
-    logger.log_info("Verifying \"" + file_path_config + "\"...")
-
-    file_generate_config = open(file_path_config, "r")
-    generate_config = json.load(file_generate_config)
-    file_generate_config.close()
-
-    for label in generate_config.keys():
-        if len(generate_config[label]["labels"]) == 0:
-            logger.log_error("\"" + label + "\" does not contain any labels.")
-        else:
-            for dataset_name in generate_config[label]["labels"].keys():
-                if generate_config[label]["labels"][dataset_name] == "":
-                    logger.log_error("\"" + label + "\" contains an empty label for dataset \"" + dataset_name + "\".")
-                elif generate_config[label]["labels"][dataset_name].split(header.dataset_delimiter_label)[0] != dataset_name:
-                    logger.log_error("\"" + label + "\" contains an invalid label for dataset \"" + dataset_name + "\".")
-
-        if len(generate_config[label]["weights"]) == 0:
-            logger.log_error("\"" + label + "\" does not contain any weights.")
-        else:
-            weights_sum = 0
-
-            for dataset_name in generate_config[label]["weights"].keys():
-                weights_sum += generate_config[label]["weights"][dataset_name]
-
-            if weights_sum == 0:
-                logger.log_error("\"" + label + "\" contains zero weights.")
-            elif weights_sum < 1:
-                logger.log_error("\"" + label + "\" contains weights summing up less than one.")
-            elif weights_sum > 1:
-                logger.log_error("\"" + label + "\" contains weights summing up greater than one.")
-
-        reduction = generate_config[label]["reduction"]
-
-        if reduction == "":
-            logger.log_error("\"" + label + "\" contains an empty reduction.")
-
-    logger.log_info_raw("\n")
+def verifyDatasetSplits():
     logger.log_info("Verifying \"" + header.verify_dataset_dir_images_split + "\"...")
 
     if os.path.isdir(header.verify_dataset_dir_images_split):
@@ -59,6 +23,66 @@ def main():
 
                 if len(dataset_dir_labels_split_list) == 0:
                     logger.log_error("\"" + dataset_dir_labels + "\" has no data for dataset split \"" + dataset_name_split + "\".")
+
+    return
+
+def verifyGenerateConfigLabels(generate_config, label):
+    if len(generate_config[label]["labels"]) == 0:
+        logger.log_error("\"" + label + "\" does not contain any labels.")
+    else:
+        for dataset_name in generate_config[label]["labels"].keys():
+            if generate_config[label]["labels"][dataset_name] == "":
+                logger.log_error("\"" + label + "\" contains an empty label for dataset \"" + dataset_name + "\".")
+            elif generate_config[label]["labels"][dataset_name].split(header.dataset_delimiter_label)[0] != dataset_name:
+                logger.log_error("\"" + label + "\" contains an invalid label for dataset \"" + dataset_name + "\".")
+
+    return
+
+def verifyGenerateConfigWeights(generate_config, label):
+    if len(generate_config[label]["weights"]) == 0:
+            logger.log_error("\"" + label + "\" does not contain any weights.")
+    else:
+        weights_sum = 0
+
+        for dataset_name in generate_config[label]["weights"].keys():
+            weights_sum += generate_config[label]["weights"][dataset_name]
+
+        if weights_sum == 0:
+            logger.log_error("\"" + label + "\" contains zero weights.")
+        elif weights_sum < 1:
+            logger.log_error("\"" + label + "\" contains weights summing up less than one.")
+        elif weights_sum > 1:
+            logger.log_error("\"" + label + "\" contains weights summing up greater than one.")
+
+    return
+
+def verifyGenerateConfigReductions(generate_config, label):
+    reduction = generate_config[label]["reduction"]
+
+    if reduction == "":
+        logger.log_error("\"" + label + "\" contains an empty reduction.")
+
+    return
+
+def verifyGenerateConfig():
+    file_path_generate_config = os.path.join(header.config_dir, header.generate_config_file_name)
+
+    logger.log_info("Verifying \"" + file_path_generate_config + "\"...")
+
+    file_generate_config = open(file_path_generate_config, "r")
+    generate_config = json.load(file_generate_config)
+    file_generate_config.close()
+
+    for label in generate_config.keys():
+        verifyGenerateConfigLabels(generate_config, label)
+        verifyGenerateConfigWeights(generate_config, label)
+        verifyGenerateConfigReductions(generate_config, label)
+
+    return
+
+def main():
+    verifyDatasetSplits()
+    verifyGenerateConfig()
 
     return
 
