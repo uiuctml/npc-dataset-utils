8f79e67dd73e5a9d9e0bbd4519e853780a23c6ef
Simon Yu <jundayu2@illinois.edu>
2023-03-07 19:23:28 -0600

    Remove filter and view header variables

--- src/header.py
+++ src/header.py
@@ -30,29 +30,12 @@ dataset_file_name_split_train = "train.txt"
 dataset_file_name_split_validate = "val.txt"
 dataset_label_undefined_keyword = "undefined"
 
-filter_dataset_dir_annotations = dataset_dir_annotations_original
-filter_file_name = "filter_" + filter_dataset_dir_annotations.split("/")[-1] + ".txt"
-filter_print_params = True
-filter_frame_ispano = 0
-filter_object_keys = []
-filter_object_labels = ["regulatory--one-way-left--g1", "regulatory--one-way-right--g1"]
-filter_object_properties = False
-filter_object_barrier = 0
-filter_object_occluded = 0
-filter_object_out_of_frame = 0
-filter_object_exterior = 0
-filter_object_ambiguous = 0
-filter_object_included = 0
-filter_object_direction_or_information = 0
-filter_object_highway = 0
-filter_object_dummy = 0
-
 generate_config_file_name = "generate.json"
-generate_dataset_dir_images = dataset_dir_images_sliced + "/original"
+generate_dataset_dir_images = dataset_dir_images_sliced_original
 
 label_combo_box_width = 250
 label_config_file_name = dataset_config_file_name
-label_dataset_dir_images = dataset_dir_images_sliced + "/original"
+label_dataset_dir_images = dataset_dir_images_sliced_original
 label_viewer_height = 150
 label_viewer_width = 150
 label_line_edit_width = 250
@@ -84,15 +67,4 @@ split_original_percentage_validate = 0.1
 split_random_seed = 80241652
 split_save = False
 
-verify_dataset_dir_images_split = dataset_dir_images_split + "/original"
-
-view_bounding_box_color = (0, 255, 0)
-view_bounding_box_text_color = (0, 0, 0)
-view_bounding_box_text_margin_x = 10
-view_bounding_box_text_margin_y = 10
-view_bounding_box_text_size = 1
-view_bounding_box_thickness = 3
-view_dataset_dir_annotations = dataset_dir_annotations_original
-view_dataset_dir_images = dataset_dir_images_original
-view_filter_file_name = "filter_" + filter_dataset_dir_annotations.split("/")[-1] + ".txt"
-view_window_height = 900
+verify_dataset_dir_images_split = dataset_dir_images_split_original
