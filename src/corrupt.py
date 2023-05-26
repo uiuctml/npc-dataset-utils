d7bcbd560aed8e1d54390a56b34cc62221f53d45
Simon Yu <jundayu2@illinois.edu>
2023-05-25 19:09:41 -0500

    Rewrite corrupt functions for parallel executions

--- src/corrupt.py
+++ src/corrupt.py
@@ -52,42 +52,42 @@ def corrupt(image, corruption, severity):
     logger.log_warn("Unknown corruption \"" + corruption.name + "\"")
     return image
 
-def corruptOriginal():
-    file_dir_class_counter = 0
-    file_path_split_test_list = os.listdir(header.dataset_dir_images_split_original_test)
-    progress_bar = tqdm.tqdm(total = len(file_path_split_test_list))
+def corruptOriginal(file_path_image_list):
+    file_path_image_counter = 0
+    progress_bar = tqdm.tqdm(total = len(file_path_image_list))
 
-    for file_dir_class in file_path_split_test_list:
-        progress_bar.set_description_str("[INFO]: Processing \"" + file_dir_class + "\"")
-        file_path_class = os.path.join(header.dataset_dir_images_split_original_test, file_dir_class)
-        progress_bar.n = file_dir_class_counter
+    for file_path_image in file_path_image_list:
+        file_dir_class = file_path_image.split("/")[-2]
+        file_key = file_path_image.split(header.dataset_file_extension_images)[0].split("/")[-1]
+        file_name_image = file_path_image.split("/")[-1]
+
+        progress_bar.set_description_str("[INFO]: Processing \"" + file_key + "\"")
+        progress_bar.n = file_path_image_counter
         progress_bar.refresh()
-        file_dir_class_counter += 1
+        file_path_image_counter += 1
+
+        image = PIL.Image.open(file_path_image)
+        image = image.resize((header.corrupt_image_size, header.corrupt_image_size))
+        image = numpy.array(image)
+
+        if header.log_level >= type.LogLevel.trace:
+            matplotlib.pyplot.imshow(image)
+            matplotlib.pyplot.show()
+
+        image_corrupted = corrupt(image, header.corrupt_corruption, header.corrupt_severity)
+
+        if header.log_level >= type.LogLevel.trace:
+            matplotlib.pyplot.imshow(image_corrupted)
+            matplotlib.pyplot.show()
+
+        image_corrupted = PIL.Image.fromarray(image_corrupted)
 
         file_dir_corruption_params = header.corrupt_corruption.name + "_" + str(header.corrupt_severity)
         file_path_class_corrupted = os.path.join(header.dataset_dir_images_split_corrupted_original_test, file_dir_corruption_params, file_dir_class)
+        file_path_image_corrupted = os.path.join(file_path_class_corrupted, file_name_image)
+
         os.makedirs(file_path_class_corrupted, exist_ok = True)
-
-        for file_name_image in os.listdir(file_path_class):
-            file_path_image = os.path.join(file_path_class, file_name_image)
-            image = PIL.Image.open(file_path_image)
-            image = image.resize((header.corrupt_image_size, header.corrupt_image_size))
-            image = numpy.array(image)
-
-            if header.log_level >= type.LogLevel.trace:
-                matplotlib.pyplot.imshow(image)
-                matplotlib.pyplot.show()
-
-            image_corrupted = corrupt(image, header.corrupt_corruption, header.corrupt_severity)
-
-            if header.log_level >= type.LogLevel.trace:
-                matplotlib.pyplot.imshow(image_corrupted)
-                matplotlib.pyplot.show()
-
-            image_corrupted = PIL.Image.fromarray(image_corrupted)
-
-            file_path_image_corrupted = os.path.join(file_path_class_corrupted, file_name_image)
-            image_corrupted.save(file_path_image_corrupted)
+        image_corrupted.save(file_path_image_corrupted)
 
     progress_bar.close()
 
@@ -95,7 +95,17 @@ def corruptOriginal():
 
 def main():
     setSeed(header.corrupt_random_seed)
-    corruptOriginal()
+
+    file_path_image_list = []
+
+    for file_dir_class in os.listdir(header.dataset_dir_images_split_original_test):
+        file_path_class = os.path.join(header.dataset_dir_images_split_original_test, file_dir_class)
+
+        for file_name_image in os.listdir(file_path_class):
+            file_path_image = os.path.join(file_path_class, file_name_image)
+            file_path_image_list.append(file_path_image)
+
+    corruptOriginal(file_path_image_list)
 
     return
 
