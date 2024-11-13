819a5ceb6f4ab7cf35df53e86e8a159d8c82cd2d
Simon Yu <jundayu2@illinois.edu>
2024-11-13 13:49:33 -0600

    Add multi-valued attribute categories as separate SPN instances

--- src/spn_instance.py
+++ src/spn_instance.py
@@ -19,25 +19,36 @@ def main():
     dirs_dataset_original = [header.dataset_dir_images_split_original_test, header.dataset_dir_images_split_original_train, header.dataset_dir_images_split_original_validate]
 
     for i in range(len(dirs_dataset_original)):
-        logger.log_info("Generating SPN dataset from \"" + dirs_dataset_original[i] + "\".")
-
         lines = []
 
         for class_name in os.listdir(dirs_dataset_original[i]):
             for instance_name in os.listdir(os.path.join(dirs_dataset_original[i], class_name)):
                 image_name = os.path.join(class_name, instance_name)
+                attribute_categories_list = [[]]
                 attributes = dataset_config["mappings"][image_name]["labels"]
                 line = ""
 
                 logger.log_debug(image_name)
 
                 for attribute_name in attributes.keys():
-                    index_label_attribute = indices_attribute[attribute_name][attributes[attribute_name]]
-                    line += str(index_label_attribute) + ","
+                    if isinstance(attributes[attribute_name], list):
+                        attribute_categories_list_new = []
 
-                index_label_original = indices_original[class_name]
-                line += str(index_label_original) + "\n"
-                lines.append(line)
+                        for category in attributes[attribute_name]:
+                            for attribute_categories in attribute_categories_list:
+                                attribute_categories_new = attribute_categories.copy()
+                                attribute_categories_new.append(str(indices_attribute[attribute_name][category]))
+                                attribute_categories_list_new.append(attribute_categories_new)
+
+                        attribute_categories_list = attribute_categories_list_new
+                    else:
+                        for attribute_categories in attribute_categories_list:
+                            attribute_categories.append(str(indices_attribute[attribute_name][attributes[attribute_name]]))
+
+                for attribute_categories in attribute_categories_list:
+                    attribute_categories.append(str(indices_original[class_name]) + "\n")
+                    line = ','.join(attribute_categories)
+                    lines.append(line)
 
         lines[-1] = lines[-1].rstrip("\n")
 
@@ -49,6 +60,8 @@ def main():
 
         with open(file_path_spn_dataset, "w+") as file_dataset_spn:
             file_dataset_spn.writelines(lines)
+
+        logger.log_info(str(len(lines)) + " SPN dataset instances generated from \"" + dirs_dataset_original[i] + "\".")
 
     return
 
