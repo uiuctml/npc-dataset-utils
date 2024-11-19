54c143ba517acc40a1eb6160f7926d19a8dbafa0
Simon Yu <jundayu2@illinois.edu>
2024-11-19 15:21:39 -0600

    Combine CUB scripts

--- src/cub.py
+++ src/cub.py
@@ -5,6 +5,31 @@ import json
 import logger
 import os
 
+attributes_whitelist = {}
+class_whitelist = {}
+
+def pruneAttributes(attributes, mappings):
+    attributes_map = {}
+
+    for attribute in attributes:
+        attribute["labels"] = [""]
+        attributes_map[attribute["name"]] = set()
+
+    for image_name in mappings.keys():
+        labels = mappings[image_name]["labels"]
+
+        for attribute_name in labels.keys():
+            if isinstance(labels[attribute_name], list):
+                for label in labels[attribute_name]:
+                    attributes_map[attribute_name].add(label)
+            else:
+                attributes_map[attribute_name].add(labels[attribute_name])
+
+    for attribute in attributes:
+        attribute["labels"] += sorted(list(attributes_map[attribute["name"]]))
+
+    return attributes
+
 def readAttributes():
     attributes = {}
     attributes_list = []
@@ -17,6 +42,9 @@ def readAttributes():
         attribute = '-'.join(line_split[1].split('_')[1:])
         attribute_split = attribute.split("::")
         attribute = header.dataset_delimiter_label.join(attribute_split)
+
+        if len(attributes_whitelist) > 0 and attribute_split[0] not in attributes_whitelist:
+            continue
 
         if attribute_split[0] not in attributes:
             attributes[attribute_split[0]] = [header.dataset_delimiter_label.join([attribute_split[0], "none"])]
@@ -37,13 +65,11 @@ def readAttributes():
     return (attributes_list, attributes_map)
 
 def readMappings(attributes):
-    blacklist = set()
     file_images = open(os.path.join(header.dataset_dir_annotations, header.cub_file_name_images), 'r')
     file_image_attribute_labels = open(os.path.join(header.cub_dir_attributes, header.cub_file_name_image_attribute_labels), 'r')
     images_map = {}
     labels_default = {}
     mappings = {}
-    mappings_filtered = {}
 
     for attribute in attributes[0]:
         labels_default[attribute["name"]] = header.dataset_delimiter_label.join([attribute["name"], "none"])
@@ -51,58 +77,62 @@ def readMappings(attributes):
     for line in file_images.readlines():
         line = line.rstrip()
         line_split = line.split(' ')
-        images_map[line_split[0]] = line_split[1]
-        mappings[line_split[1]] = {"labels": labels_default.copy()}
+        image_name = line_split[1]
+        class_name = image_name.split('/')[0]
+
+        if len(class_whitelist) > 0 and class_name not in class_whitelist:
+            continue
+
+        images_map[line_split[0]] = image_name
+        mappings[image_name] = {"labels": labels_default.copy()}
 
     file_images.close()
 
     for line in file_image_attribute_labels.readlines():
         line = line.rstrip()
         line_split = line.split(' ')
-        attribute_id = line_split[1]
-        attribute_present = line_split[2]
-        certainty_id = line_split[3]
-        image_id = line_split[0]
-        image_name = images_map[image_id]
 
-        if image_name in blacklist:
+        if line_split[0] not in images_map:
             continue
 
-        if header.cub_filter_by_certainty and int(certainty_id) < 3:
-            blacklist.add(image_name)
+        image_name = images_map[line_split[0]]
+        class_name = image_name.split('/')[0]
+        file_name = image_name.split('/')[1]
+
+        if len(class_whitelist) > 0 and class_name not in class_whitelist:
             continue
 
-        if attribute_present == "0":
+        if line_split[2] == "0":
             continue
 
-        logger.log_info_raw("[INFO]: Reading mappings for \"" + image_name + "\"", end = '\r')
+        logger.log_info_raw("\033[K[INFO]: Reading mappings for \"" + file_name + "\"", end = '\r')
 
-        attribute = attributes[1][attribute_id]
+        if line_split[1] not in attributes[1]:
+            continue
+
+        attribute = attributes[1][line_split[1]]
         attribute_name = attribute.split(header.dataset_delimiter_label)[0]
 
-        if mappings[image_name]["labels"][attribute_name] == header.dataset_delimiter_label.join([attribute_name, "none"]):
-            mappings[image_name]["labels"][attribute_name] = attribute
-        elif header.cub_filter_by_attribute_uniqueness:
-            blacklist.add(image_name)
-            continue
+        if isinstance(mappings[image_name]["labels"][attribute_name], list):
+            mappings[image_name]["labels"][attribute_name].append(attribute)
+        else:
+            if mappings[image_name]["labels"][attribute_name] == header.dataset_delimiter_label.join([attribute_name, "none"]):
+                mappings[image_name]["labels"][attribute_name] = attribute
+            else:
+                mappings[image_name]["labels"][attribute_name] = [mappings[image_name]["labels"][attribute_name], attribute]
 
     file_image_attribute_labels.close()
 
     logger.log_info_raw()
-    logger.log_info("Filtered " + str(len(blacklist)) + "/" + str(len(images_map)) + " images.")
 
-    for image_name in mappings.keys():
-        if image_name not in blacklist:
-            mappings_filtered[image_name] = mappings[image_name]
-
-    return mappings_filtered
+    return mappings
 
 def main():
     attributes = readAttributes()
     mappings = readMappings(attributes)
     config = {}
     config["instance_wise"] = True
-    config["attributes"] = attributes[0]
+    config["attributes"] = pruneAttributes(attributes[0], mappings)
     config["mappings"] = mappings
 
     with open(os.path.join(header.config_dir, header.dataset_config_file_name), 'w') as file_config:
