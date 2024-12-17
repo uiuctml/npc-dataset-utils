304216f52b7b7f4cd0e4ae10f3e173fdf273bf55
Simon Yu <jundayu2@illinois.edu>
2024-12-17 17:01:25 -0600

    Assign classes

--- src/celeba.py
+++ src/celeba.py
@@ -31,6 +31,78 @@ attribute_types = {
     ],
 }
 
+def generateLabeledDatasetDirectory(mappings):
+    if os.path.exists(header.celeba_dataset_dir_images_labeled):
+        logger.log_info("Labeled dataset directory exists. Skip.")
+        return
+
+    for instance_name in mappings:
+        logger.log_info_raw("\033[K[INFO]: Processing instance \"" + instance_name + "\"", end = '\r')
+
+        instance_name_split = instance_name.split('/')
+        class_name = instance_name_split[0]
+        file_name = instance_name_split[1]
+        dir_class_sliced =  os.path.join(header.dataset_dir_images_sliced, class_name)
+        dir_class_labeled = os.path.join(header.celeba_dataset_dir_images_labeled, class_name)
+
+        os.makedirs(dir_class_labeled, exist_ok = True)
+        os.symlink(os.path.abspath(os.path.join(dir_class_sliced, file_name)), os.path.join(dir_class_labeled, file_name))
+
+    logger.log_info_raw()
+
+    return
+
+def assignClasses(mappings):
+    attribute_assignments = set()
+    attribute_assignments_map = {}
+    class_assignment = 0
+    class_assignments_map = {}
+    mappings_assigned = {}
+
+    for instance_name in mappings.keys():
+        logger.log_info_raw("\033[K[INFO]: Processing instance \"" + instance_name + "\"", end = '\r')
+
+        attribute_assignment = ""
+
+        for attribute_name in mappings[instance_name]["labels"].keys():
+            attribute_values = mappings[instance_name]["labels"][attribute_name]
+
+            if isinstance(attribute_values, list):
+                for attribute_value in attribute_values:
+                    attribute_assignment += attribute_value
+            else:
+                attribute_assignment += attribute_values
+
+        attribute_assignments.add(attribute_assignment)
+        attribute_assignments_map[instance_name] = attribute_assignment
+
+    logger.log_info_raw()
+
+    attribute_assignments = list(attribute_assignments)
+    attribute_assignments = utility.shuffleUniform(attribute_assignments, header.celeba_random_seed)
+    attribute_assignments = list(zip(attribute_assignments[::2], attribute_assignments[1::2]))
+
+    for attribute_assignment_pair in attribute_assignments:
+        class_assignments_map[attribute_assignment_pair[0]] = class_assignment
+        class_assignments_map[attribute_assignment_pair[1]] = class_assignment
+        class_assignment += 1
+
+    for instance_name in mappings.keys():
+        logger.log_info_raw("\033[K[INFO]: Processing instance \"" + instance_name + "\"", end = '\r')
+
+        attribute_assignment = attribute_assignments_map[instance_name]
+        class_assignment = class_assignments_map[attribute_assignment]
+        labels = mappings[instance_name]["labels"]
+        instance_name = str(class_assignment) + '/' + instance_name
+
+        mappings_assigned[instance_name] = {"labels": labels}
+
+    logger.log_info_raw()
+
+    generateLabeledDatasetDirectory(mappings_assigned)
+
+    return mappings_assigned
+
 def computeAttributeBalanceScores(matrix, attributes):
     balance_scores = {}
 
@@ -179,10 +251,11 @@ def main():
     (matrix, attributes) = readMatrix()
     attributes = processAttributes(matrix, attributes)
     mappings = processMappings(matrix, attributes)
+    mappings = assignClasses(mappings)
     config = {}
     config["instance_wise"] = True
     config["attributes"] = attributes
-    # TODO config["attributes"] = utility.pruneAttributes(attributes, mappings)
+    config["attributes"] = utility.pruneAttributes(attributes, mappings)
     config["mappings"] = mappings
 
     with open(os.path.join(header.config_dir, header.dataset_config_file_name), 'w') as file_config:
--- src/header.py
+++ src/header.py
@@ -40,7 +40,9 @@ awa_file_name_matrix = "predicate-matrix-binary.txt"
 awa_file_name_predicates = "predicates.txt"
 
 celeba_count_attributes = 8
+celeba_dataset_dir_images_labeled = dataset_dir_images + "/labeled"
 celeba_file_name_attributes = "list_attr_celeba.txt"
+celeba_random_seed = 42
 
 cub_dataset_dir_images_filtered = dataset_dir_images + "/filtered"
 cub_dir_attributes = dataset_dir_annotations + "/attributes"
